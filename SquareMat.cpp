e2a5bb416bdf507af913e9db158e1feeb3990a71
Adi <12adi45@gmail.com>
2025-06-09 23:39:49 +0300

    09.06 | 23:40

--- SquareMat.cpp
+++ SquareMat.cpp
@@ -1,8 +1,430 @@
 //
-// Created by 12adi on 08/06/2025.
+// Created by 12adi on 09/06/2025.
 //
 
 #include "SquareMat.h"
+#include <stdexcept>
 
-namespace matrix {
+namespace matrix
+{
+SquareMat::SquareMat(int size): size(size)
+{
+  if (size <= 0){
+    throw std::invalid_argument("Size must be greater than 0");
+  }
+  matrix = new double*[size];
+  for (int i = 0; i < size; i++){
+    matrix[i] = new double[size];
+  }
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      matrix[i][j] = 0;
+    }
+  }
+}
+
+SquareMat::~SquareMat()
+{
+  for (int i = 0; i < size; i++)
+  {
+    delete[] matrix[i];
+  }
+  delete [] matrix;
+}
+
+SquareMat::SquareMat(const SquareMat &other): size(other.size)
+{
+  matrix = new double*[size];
+  for (int i = 0; i < size; i++)
+  {
+    matrix[i] = new double[size];
+  }
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      matrix[i][j] = other.matrix[i][j];
+    }
+  }
+}
+
+SquareMat &SquareMat::operator=(const SquareMat &other)
+{
+  if (this != &other)
+  {
+    if (this->size != other.size)
+    {
+      for (int i = 0; i < size; i++)
+      {
+        delete[] matrix[i];
+      }
+      delete [] matrix;
+      size = other.size;
+      matrix = new double*[size];
+      for (int i = 0; i < size; i++)
+      {
+        matrix[i] = new double[size];
+      }
+    }
+    for (int i = 0; i < size; i++)
+    {
+      for (int j = 0; j < size; j++)
+      {
+        matrix[i][j] = other.matrix[i][j];
+      }
+    }
+  }
+  return *this;
+}
+
+  // חיבור וחיסור מטריצות בגדלים זהים
+SquareMat SquareMat::operator+(const SquareMat& m) const
+{
+  if (this->size != m.size)
+  {
+    throw std::invalid_argument("Matrixs size must be same");
+  }
+
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = this->matrix[i][j] + m.matrix[i][j];
+    }
+  }
+  return result;
+}
+
+SquareMat SquareMat::operator-(const SquareMat& m) const
+{
+  if (this->size != m.size)
+  {
+    throw std::invalid_argument("Matrixs size must be same");
+  }
+
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = this->matrix[i][j] - m.matrix[i][j];
+    }
+  }
+  return result;
+}
+
+  // שינוי הסימן של מקדם המטריצה
+SquareMat SquareMat::operator-() const
+{
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = -(this->matrix[i][j]);
+    }
+  }
+  return result;
+}
+
+  //הכפלה בין 2 מטריצות
+SquareMat SquareMat::operator*(const SquareMat& m) const
+{
+  if (this->size != m.size)
+  {
+    throw std::invalid_argument("Matrixs size must be same");
+  }
+
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      for (int k = 0; k < size; k++)
+      {
+        result.matrix[i][j] += this->matrix[i][k] * m.matrix[k][j];
+      }
+    }
+  }
+  return result;
+}
+
+  // הכפלת מטריצה בסקלר
+SquareMat operator*(double scalar, const SquareMat& m)
+{
+  SquareMat result = SquareMat(m.size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = scalar * m.matrix[i][j];
+    }
+  }
+}
+
+SquareMat SquareMat::operator*(double scalar) const
+{
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = scalar * this->matrix[i][j];
+    }
+  }
+  return result;
+}
+
+  // הכפלה בין 2 מטריצות בצורה לא רגילה - כלומר ההכפלה תעשה איבר במיקום ה i,j עם האיבר במיקום ה i,j מהמטריצה השניה
+SquareMat SquareMat::operator%(const SquareMat& m) const
+{
+  if (this->size != m.size)
+  {
+    throw std::invalid_argument("Matrixs size must be same");
+  }
+
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = this->matrix[i][j] * m.matrix[i][j];
+    }
+  }
+  return result;
+}
+
+  // מבצע פעולה של מודולו על כל איבר במטריצה עם מספר שלם נתון
+SquareMat SquareMa::operator%(double scalar) const
+{
+  if(scalar <= 0)
+  {
+    throw std::invalid_argument("Number must be greater than 0");
+  }
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = std::fmod(this->matrix[i][j], scalar);
+    }
+  }
+  return result;
+}
+
+  // מחלק כל איבר במטריצה במספר סקלרי
+SquareMat SquareMat::operator/(double scalar) const
+{
+  if (scalar == 0)
+  {
+    throw std::invalid_argument("Number must be different than 0");
+  }
+  SquareMat result = SquareMat(this->size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      result.matrix[i][j] = this->matrix[i][j] / scalar;
+    }
+  }
+  return result;
+}
+
+  // אופרטור חזקה
+SquareMat SquareMat::operator^(int exponent) const
+{
+  SquareMat result(size);
+  for (int i = 0; i < size; i++)
+  {
+    result.matrix[i][i] = 1;
+  }
+  SquareMat current(*this);
+  if (exponent == 0)
+  {
+    return result;
+  }
+  else if (exponent < 0)
+  {
+    exponent = -exponent;
+    while (exponent > 0)
+    {
+      if(exponent % 2 == 1)
+      {
+        result = result * current;
+      }
+      current = current * current;
+      exponent = exponent / 2;
+    }
+    return 1/result;
+  }
+
+  else
+  {
+    while (exponent > 0)
+    {
+      if(exponent % 2 == 1)
+      {
+        result = result * current;
+      }
+      current = current * current;
+      exponent = exponent / 2;
+    }
+  }
+  return result;
+}
+
+  // אופרטורי הגדלה והקטנה ב-1
+SquareMat &SquareMat::operator++()
+{
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      this->matrix[i][j] += 1;
+    }
+  }
+  return *this;
+} // prefix
+SquareMat &SquareMat::operator++(int)
+{
+  SquareMat result = this->matrix;
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      this->matrix[i][j] += 1;
+    }
+  }
+  return *result;
+} // postfix
+  SquareMat &SquareMat::operator--()
+{
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      this->matrix[i][j] -= 1;
+    }
+  }
+  return *this;
+} // prefix
+  SquareMat &SquareMat::operator--(int)
+{
+  SquareMat result = this->matrix;
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      this->matrix[i][j] -= 1;
+    }
+  }
+  return *result;
+} // postfix
+
+  //מחליף את השורות בעמודות של המטריצה
+SquareMat SquareMat::operator~() const
+{
+  SquareMat result(size);
+  for (int i = 0; i < size; i++)
+  {
+     for (int j = 0; j < size; j++)
+    {
+       result.matrix[i][j] = this->matrix[j][i];
+    }
+  }
+  return result;
+}
+
+  // אופרטור גישה לאיברים באמצעות אינדקס
+double* SquareMat::operator[](int i)
+{
+  if (i < 0 || i >= size)
+  {
+    throw std::invalid_argument("Index out of bounds");
+  }
+  return matrix[i];
+}
+
+const double* SquareMat::operator[](int i) const
+{
+  if (i < 0 || i >= size)
+  {
+    throw std::invalid_argument("Index out of bounds");
+  }
+  return matrix[i];
+}
+
+  //אופרטורי שוויון ואי-שוויון
+bool SquareMat::operator==(const SquareMat& m) const
+{
+  double sumThis = 0;
+  double sumM = 0;
+
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      sumThis += this->matrix[i][j];
+      sumM += m.matrix[i][j];
+    }
+  }
+  return sumThis == sumM;
+}
+bool SquareMat::operator!=(const SquareMat& m) const
+{
+  return !(*this == m);
+}
+
+  //אופרטורי השוואה
+bool SquareMat::operator<(const SquareMat& m) const
+{
+  {
+    double sumThis = 0;
+    double sumM = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+      for (int j = 0; j < size; j++)
+      {
+        sumThis += this->matrix[i][j];
+        sumM += m.matrix[i][j];
+      }
+    }
+    return sumThis < sumM;
+  }
+
+}
+bool SquareMat::operator>=(const SquareMat& m) const
+{
+  return !(*this < m);
+}
+bool SquareMat::operator>(const SquareMat& m) const
+{
+  return (m < *this);
+}
+bool SquareMat::operator<=(const SquareMat& m) const
+{
+  return !(m < *this);
+}
+
+  //מחשב את הדטרמיננטה של המטריצה
+double SquareMat::operator!() const
+{
+
+}
+
+  //אופרטורי השמה משולבים
+SquareMat &SquareMat::operator+=(const SquareMat& m){}
+SquareMat &SquareMat::operator-=(const SquareMat& m){}
+SquareMat &SquareMat::operator*=(const SquareMat& m){}
+SquareMat &SquareMat::operator%=(const SquareMat& m){}
+SquareMat &SquareMat::operator/=(const SquareMat& m){}
+
+  // מדפיס את המטריצה בצורה הגיונית
+  friend std::ostream& operator<<(std::ostream& os, const SquareMat& m);
+
+
 } // matrix
--- SquareMat.h
+++ SquareMat.h
@@ -1,31 +1,85 @@
 //
-// Created by 12adi on 08/06/2025.
+// 12adi45@gmail.com
 //
 
 #ifndef SQUAREMAT_H
 #define SQUAREMAT_H
+#include <cstdio>
 
 namespace matrix {
 
 class SquareMat {
-  Matrix Addition/SubtractionOperator(matrix a, matrix b, string action); // חיבור וחיסור מטריצות בגדלים זהים
-  void MinusOneOperator(matrix a); // שינוי הסימן של מקדם המטריצה
-  Matrix MultiplicationOperator(matrix a, matrix b); //הכפלה בין 2 מטריצות
-  void ScalarMultiplication(matrix a, int b); // הכפלת מטריצה בסקלר ממשי!!
-  Matrix MultiplicationOfOrgans(); //
-  Matrix ModuluWithScalar(matrix a, int num); // מבצע פעולה של מודולו על כל איבר במטריצה עם מספר שלם נתון
-  Matrix ScalarDivision(matrix a, int num); // מחלק כל איבר במטריצה במספר סקלרי
-  Matrix StrongOperator(matrix a, int hezka); // אופרטור חזקה
-  void plossAndMinusByOneOperator(matrix a); // אופרטורי הגדלה והקטנה ב-1
-  void TransposeOperator(matrix a); //מחליף את השורות בעמודות של המטריצה
-  void IndexOperator(matrix a, int row, int column); // אופרטור גישה לאיברים באמצעות אינדקס
-  void EqualityAndInequalityOperator(matrix a, matrix b); //אופרטורי שוויון ואי-שוויון
-  void ComparisonOperator(matrix a, matrix b); //אופרטורי השוואה
-  void DeterminantOperator(matrix a); //מחשב את הדטרמיננטה של המטריצה
-  void CombinedPlacementOperators(matrix a, matrix b); //אופרטורי השמה משולבים
-  void OutputOperator(matrix a); // מדפיס את המטריצה בצורה הגיונית
-};
+    public:
+      double** matrix;
+      int size;//cols&rows
 
+      SquareMat(int size); // copy constructor - העתקת אובייקט (בנאי)
+      ~SquareMat(); //destructor - לשחרור זיכרון
+      SquareMat(const SquareMat& other);
+      SquareMat& operator=(const SquareMat& mat); // copy assignment operation - השוואה בין שני אובייקטים קיימים
+
+      // חיבור וחיסור מטריצות בגדלים זהים
+      SquareMat operator+(const SquareMat& m) const;
+      SquareMat operator-(const SquareMat& m) const;
+
+      // שינוי הסימן של מקדם המטריצה
+      SquareMat operator-() const;
+
+      //הכפלה בין 2 מטריצות
+      SquareMat operator*(const SquareMat& m) const;
+
+      // הכפלת מטריצה בסקלר
+      friend SquareMat operator*(double scalar, const SquareMat& m);
+      SquareMat operator*(double scalar) const;
+
+      // הכפלה בין 2 מטריצות בצורה לא רגילה - כלומר ההכפלה תעשה איבר במיקום ה i,j עם האיבר במיקום ה i,j מהמטריצה השניה
+      SquareMat operator%(const SquareMat& m) const;
+
+      // מבצע פעולה של מודולו על כל איבר במטריצה עם מספר שלם נתון
+      SquareMat operator%(double scalar) const;
+
+       // מחלק כל איבר במטריצה במספר סקלרי
+  	  SquareMat operator/(double scalar) const;
+
+      // אופרטור חזקה
+   	  SquareMat operator^(int exponent) const;
+
+      // אופרטורי הגדלה והקטנה ב-1
+      SquareMat& operator++(); // prefix
+      SquareMat& operator++(int); // postfix
+      SquareMat& operator--(); // prefix
+      SquareMat& operator--(int); // postfix
+
+      //מחליף את השורות בעמודות של המטריצה
+  	  SquareMat operator~() const;
+
+      // אופרטור גישה לאיברים באמצעות אינדקס
+  	  double* operator[](int i);
+      const double* operator[](int i) const;
+
+      //אופרטורי שוויון ואי-שוויון
+  	  bool operator==(const SquareMat& m) const;
+  	  bool operator!=(const SquareMat& m) const;
+
+      //אופרטורי השוואה
+      bool operator>(const SquareMat& m) const;
+      bool operator>=(const SquareMat& m) const;
+      bool operator<(const SquareMat& m) const;
+      bool operator<=(const SquareMat& m) const;
+
+      //מחשב את הדטרמיננטה של המטריצה
+  	  double operator!() const;
+
+      //אופרטורי השמה משולבים
+  	  SquareMat& operator+=(const SquareMat& m);
+      SquareMat& operator-=(const SquareMat& m);
+      SquareMat& operator*=(const SquareMat& m);
+      SquareMat& operator%=(const SquareMat& m);
+      SquareMat& operator/=(const SquareMat& m);
+
+      // מדפיס את המטריצה בצורה הגיונית
+  	  friend std::ostream& operator<<(std::ostream& os, const SquareMat& m);
+	};
 } // matrix
 
 #endif //SQUAREMAT_H
